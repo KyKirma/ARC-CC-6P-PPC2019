7a82c81162758270aa694473fb8601713de5f29d
kykirma <pedrohenriquekourly@hotmail.com>
2024-11-07 09:19:28 -0300

    guess game 2.0

--- 1_intro_to_py/guess_game.py
+++ 1_intro_to_py/guess_game.py
@@ -3,12 +3,27 @@ print("Bem vindo ao jogo de Adivinhação!")
 print("*********************************\n")
 
 secret_number = 42
-guess = input("Digite o seu número: ")
-print("Você digitou: ", guess)
+tries = 3
+run = 1
 
-if secret_number == int(guess): 
-    print("Acertou!! :D")
-else: 
-    print("Errou! :C")
+while tries:
+    print("\nTentativa", run, "de", tries)
+    guess_str = input("Qual o seu chute? ")
+    guess = int(guess_str)
 
-print ("Fim do jogo")
+    correct = guess == secret_number
+    bigger = guess > secret_number
+    smaller = guess < secret_number
+
+    if correct:
+        print("Acertou!! :D")
+    else:
+        if bigger:
+            print("Errou! :C  O seu chute foi maior que o número secreto.")
+        elif smaller:
+            print("Errou! :C  O seu chute foi menor que o número secreto.")
+
+    run += 1
+    tries -= 1
+
+print("\nFim do Jogo!")
